bd23ac8b77a86d066e6ad0f3ff06a8dce53881ef
sdesousa <sylvaindesousa@free.fr>
2021-03-10 18:13:00 +0100

    serialization fixtures fix

--- src/DataFixtures/MangaFixtures.php
+++ src/DataFixtures/MangaFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Editor;
 use App\Entity\Manga;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker;
 
 class MangaFixtures extends Fixture implements DependentFixtureInterface
--- src/DataFixtures/MangaRecordFixtures.php
+++ src/DataFixtures/MangaRecordFixtures.php
@@ -7,7 +7,7 @@ use App\Entity\MangaRecord;
 use App\Entity\Record;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker;
 
 class MangaRecordFixtures extends Fixture implements DependentFixtureInterface
