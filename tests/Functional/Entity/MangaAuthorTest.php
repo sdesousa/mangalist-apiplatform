80efd9ecd6cfd6333577da3c32d761ba83331619
sdesousa <sylvaindesousa@free.fr>
2021-07-04 16:04:35 +0200

    functional tests

---
+++ tests/Functional/Entity/MangaAuthorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Functional\Entity;
+
+use App\Tests\Functional\AbstractEndPoint;
+use JsonException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MangaAuthorTest extends AbstractEndPoint
+{
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetMangaAuthors(): void
+    {
+        $response = $this->getResponseFromRequest(Request::METHOD_GET, '/api/manga_authors');
+        $responseContent = $response->getContent();
+
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        if ($responseContent) {
+            $responseDecoded = json_decode($responseContent, true, 512, JSON_THROW_ON_ERROR);
+            self::assertJson($responseContent);
+            self::assertNotEmpty($responseDecoded);
+        }
+    }
+}
---
+++ tests/Functional/Entity/MangaRecordTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Functional\Entity;
+
+use App\Tests\Functional\AbstractEndPoint;
+use JsonException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MangaRecordTest extends AbstractEndPoint
+{
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetMangaRecords(): void
+    {
+        $response = $this->getResponseFromRequest(Request::METHOD_GET, '/api/manga_records');
+        $responseContent = $response->getContent();
+
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        if ($responseContent) {
+            $responseDecoded = json_decode($responseContent, true, 512, JSON_THROW_ON_ERROR);
+            self::assertJson($responseContent);
+            self::assertNotEmpty($responseDecoded);
+        }
+    }
+}
